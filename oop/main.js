50d95a9171d85190343b69891847b973c4ce562f
Szikora Balint <balint.szikora00@gmail.com>
2025-04-14 17:09:24 +0200

    tablazat letrehozasa oop

--- oop/main.js
+++ oop/main.js
@@ -1,5 +1,5 @@
 const elvalaszto = document.createElement('hr'); // hogy a html-en egyszeruen megtalalhato legyen az elvalaszto oop es sima kozott
  document.body.appendChild(elvalaszto); // hozzáadjk a bodyhoz az elválasztó vonalat
  
- const table = new Area('table'); // létrehoz egy új 'table' típusú Area objektumot
- const form = new Area('form'); // ugyanaz mint fent, csak 'form' névvel
+const table = new Table('table'); // létrehozunk egy új 'Table' osztálypéldányt 
+const form = new Area('form'); // létrehozunk egy új osztálypéldányt 
