0e0b14cce6595614219b3ba16fd0ba2161fd1cc8
Szikora Balint <balint.szikora00@gmail.com>
2025-04-15 20:19:57 +0200

    letoltes hozzaadasa oop

--- oop/manager.js
+++ oop/manager.js
@@ -32,4 +32,16 @@ class Manager { // egy Manager nevű osztály, kezeli az adatokat
         this.#array.push(szerzo); // belerakjuk az új adatot a privát tömbbe
         this.#addSzerzoCallback(szerzo); // meghívjuk a callbacket az új adattal
     }
+
+    /**
+     * @returns {string} letöltési szöveg
+     */
+    generateOutputString() { // a letöltéshez szükséges szöveget generaljuk itt
+        const tartalomTomb = ['szerző;műfaj;cím'] // létrehozunk egy tömböt fejléc sorral
+        
+        for(const mu of this.#array){ // végigmegyünk az adatokon
+            tartalomTomb.push(`${mu.szerzo};${mu.mufaj};${mu.cim}`); // összefűzzük az adatokat és betoljuk a tömbbe
+        }
+        return tartalomTomb.join('\n'); // a sorokat egy szöveggé fűzzük össze sortöréssel elválasztva
+    }
 }
